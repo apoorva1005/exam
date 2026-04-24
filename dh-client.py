744c11790087ae5300e9f5da046a6ac876e546e7
apoorva1005 <apoorvabellam@gmail.com>
2026-04-24 13:26:44 +0530

    updated files

--- dh-client.py
+++ dh-client.py
@@ -2,20 +2,16 @@
 p = 23
 g = 5
 
-# Private keys
+# Client private key
 x = int(input("Enter client private key (x): "))
-y = int(input("Enter server private key (y): "))
 
-# Public keys
-R1 = pow(g, x, p)   # g^x mod p
-R2 = pow(g, y, p)   # g^y mod p
+# Client public key
+R1 = pow(g, x, p)
+print("Client public key (R1):", R1)
 
-print("R1 (Client public):", R1)
-print("R2 (Server public):", R2)
+# Receive server public key
+R2 = int(input("Enter server public key (R2): "))
 
 # Shared secret key
-K1 = pow(R2, x, p)  # (R2)^x mod p
-K2 = pow(R1, y, p)  # (R1)^y mod p
-
-print("Client Secret Key:", K1)
-print("Server Secret Key:", K2)
+K = pow(R2, x, p)
+print("Client Secret Key:", K)
